7f3e7be7cc0b20df0c201de8ad5effb9014bcfbf
Ravina429 <95204556+Ravina429@users.noreply.github.com>
2022-03-06 14:21:22 +0530

    Add files via upload

---
+++ 1sort.cs
@@ -0,0 +1,40 @@
+﻿//using System;
+//using System.Collections;
+
+//    namespace collection
+//{
+//    class program
+//    {
+//        static void Main()
+//        {
+//            ArrayList al = new ArrayList();
+
+//                Console.WriteLine("adding some numbers:");
+
+//            al.Add(45);
+//            al.Add(32);
+//            al.Add(21);
+//            al.Add(76);
+//            al.Add(87);
+//            al.Add(93);
+            
+//            Console.WriteLine("capacity:{0}",al.Capacity);
+//            Console.WriteLine("count:{0}", al.Count);
+//            Console.Write("content:");
+//            foreach(int i in al)
+//            {
+//                Console.Write(i + " ");
+//            }
+//            Console.WriteLine();
+//            Console.Write("sorted content:");
+//            al.Sort();
+//            foreach(int i in al)
+//            {
+//                Console.Write(i + " ");
+//            }
+//            Console.WriteLine();
+//            Console.ReadKey();
+//        }
+//    }
+//}
+
